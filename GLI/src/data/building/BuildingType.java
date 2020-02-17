ad28dc89ec20ad5df7b0c91f7ec87dcf3e236847
R-DURSO <rdurso@outlook.fr>
2020-02-17 12:02:39 +0100

    premier jet de construction de batiment

---
+++ GLI/src/data/building/BuildingType.java
@@ -0,0 +1,17 @@
+package data.building;
+
+public class BuildingType {
+	public static final int NO_CONSTRUCT = 0;
+	public static final int BUILDING_BARRACK = 1;
+	public static final int BUILDING_STABLE = 2;
+	public static final int BUILDING_DOCK = 3;
+	public static final int BUILDING_MIME = 4;
+	
+	public static final int BUILDING_SWAMILL = 5;
+	
+	public static final int BUILDING_WINDMILL = 6;
+	public static final int BUILDING_DOOR = 7;
+	public static final int BUILDING_TEMPLE =8 ;
+	public static final int BUILDING_WALL = 9;
+
+}
--- GLI/src/process/management/BuildingFactory.java
+++ GLI/src/process/management/BuildingFactory.java
@@ -1,9 +1,61 @@
 package process.management;
+import data.boxes.GroundBox;
+import data.building.Building;
+import data.building.army.*;
 
+import data.building.product.*;
+import data.building.special.*;
+import data.Power;
 public class BuildingFactory {
 
-	public BuildingFactory() {
-		// TODO Auto-generated constructor stub
+	public BuildingFactory(Power name, int type, GroundBox b) {
+		boolean result;
+		Building r ;
+		result =constructableCase( b , name.getName());
+		r=construction(result, type);
+		b.setBuilding(r);
 	}
-
+	
+	public  boolean  constructableCase(GroundBox b, String name) {
+		if(b.getBuilding()!= null && b.getOwner().equals(name)) {
+			return false;			
+		}else {
+			return true;
+		}
+	}
+	public Building construction(boolean r, int cons) {
+		if(r==true) {
+			switch(cons) {
+			case 1:
+				Building b = new Barrack();
+				return b;
+			case 2 :
+				Building a= new Stable();
+				return a;
+			case 3:
+				Building c = new Dock();
+				return c;
+			case 4:
+				Building e = new Mine();
+				return e;
+			case 5:
+				Building f = new Sawmill();
+				return f;
+			case 6:
+				Building  g = new Windmill();
+				return g;
+			case 7:
+				Building h = new Door();
+				return h;
+			case 8:
+				Building i= new Temple();
+				return i;
+			case 9:
+				Building j= new Wall();
+				return j;
+			}
+			
+		}
+		return null;
+	}
 }
