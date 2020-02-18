c5557de4730cbc9b7deeea6828bd73f3bfb2a6ac
R-DURSO <60429677+R-DURSO@users.noreply.github.com>
2020-02-18 19:09:09 +0100

    Add files via upload
    des fois mon eclipse but xd

--- GLI/src/GUI/Game.java
+++ GLI/src/GUI/Game.java
@@ -1,6 +1,7 @@
 package GUI;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -10,106 +11,101 @@ import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import javax.swing.JButton;
 
-public class Game extends JFrame implements Runnable{
+public class Game extends JFrame {
 
 	private JPanel contentPane;
 	private final JLabel map = new JLabel("affichage de la map");
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Game frame = new Game();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-	public class test implements Runnable{
-
-		@Override
-		public void run() {
-			// TODO Auto-generated method stub
-			
-		}
-	}
-	
+/*
+* 
+*/
+	private static final Dimension IDEAL_MAIN_DIMENSION = new Dimension(800,400);
+	JLabel position = new JLabel("position");
+	JLabel Action = new JLabel("action");
+	JLabel gold = new JLabel("or");
+	JLabel wood = new JLabel("bois");
+	JButton EndButton = new JButton("Quitter la partie");	
+	JLabel Stone = new JLabel("pierre");
+	JLabel food = new JLabel("nouriture");	
+	JLabel action_ReadyComboBox = new JLabel("on en fait des liste avec bouton ou bien un menu ?");	
+	JLabel label = new JLabel("New label");	
+	JLabel table = new JLabel("tableau ou le r\u00E9sumer et marquer");
+	JLabel action_doComboBox = new JLabel("on en fait des liste avec bouton ou bien un menu ?");
+	JLabel cost = new JLabel("cout btaiment/unit\u00E9");		
+	JButton end_TurnButton = new JButton("fin de tour");	
 	
 	/**
 	 * Create the frame.
 	 */
 	public Game() {
+		super("Conquête");
+		init();		
+	}
+	private void init() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 960, 505);
+		setPreferredSize(IDEAL_MAIN_DIMENSION);
+		setBounds(100, 100, 967, 503);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel position = new JLabel("position");
+		
 		position.setBounds(0, 0, 77, 40);
 		contentPane.add(position);
 		
-		JLabel Action = new JLabel("action");
+		
 		Action.setBounds(85, 0, 54, 40);
 		contentPane.add(Action);
 		
-		JLabel gold = new JLabel("or");
+
 		gold.setBounds(138, 0, 54, 40);
 		contentPane.add(gold);
 		
-		JLabel wood = new JLabel("bois");
+
 		wood.setBounds(194, 0, 54, 40);
 		contentPane.add(wood);
 		
-		JLabel Stone = new JLabel("pierre");
+		
 		Stone.setBounds(252, 0, 54, 40);
 		contentPane.add(Stone);
-		
-		JLabel food = new JLabel("nouriture");
+
 		food.setBounds(313, 0, 54, 40);
 		contentPane.add(food);
 		
-		JLabel action_Ready = new JLabel("on en fait des liste avec bouton ou bien un menu ?");
-		action_Ready.setBounds(368, 0, 578, 40);
-		contentPane.add(action_Ready);
+
+		action_ReadyComboBox.setBounds(368, 0, 578, 40);
+		contentPane.add(action_ReadyComboBox);
 		
-		JLabel label = new JLabel("New label");
+
 		label.setBounds(-20, 26, 17, 14);
 		contentPane.add(label);
 		map.setHorizontalAlignment(SwingConstants.CENTER);
 		map.setBounds(-10, 42, 778, 303);
 		contentPane.add(map);
 		
-		JLabel table = new JLabel("tableau ou le r\u00E9sumer et marquer");
+
 		table.setHorizontalAlignment(SwingConstants.TRAILING);
 		table.setBounds(775, 36, 171, 303);
 		contentPane.add(table);
+
+		end_TurnButton.setBounds(0, 356, 209, 101);
+		contentPane.add(end_TurnButton);
 		
-		JButton end_Turn = new JButton("fin de tour");
-		end_Turn.setBounds(0, 356, 209, 101);
-		contentPane.add(end_Turn);
-		
-		JLabel cost = new JLabel("cout btaiment/unit\u00E9");
-		cost.setBounds(538, 356, 200, 101);
+
+		cost.setBounds(538, 356, 161, 101);
 		contentPane.add(cost);
+		action_doComboBox.setBounds(219, 363, 290, 86);
+		contentPane.add(action_doComboBox);
 		
-		JLabel action_Ready_1 = new JLabel("on en fait des liste avec bouton ou bien un menu ?");
-		action_Ready_1.setBounds(219, 363, 290, 86);
-		contentPane.add(action_Ready_1);
-		
-		JButton End = new JButton("Quitter la partie");
-		End.setBounds(700, 356, 246, 112);
-		contentPane.add(End);
+
+		EndButton.setBounds(700, 356, 246, 112);
+		contentPane.add(EndButton);
+		setVisible(true);
 	}
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		
+	private void udapteValues() {
+		/*
+		 * on écrira ici les modfication des valeur de ressource ect ....
+		 */
 	}
 }
