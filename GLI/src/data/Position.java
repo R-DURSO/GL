b844660a09337122da8ac1dd0285a6cf53aa0d07
AldricVS <aldric.silvestre@outlook.fr>
2020-02-19 15:29:09 +0100

    changement rapide

---
+++ GLI/src/data/Position.java
@@ -0,0 +1,29 @@
+package data;
+
+public class Position {
+	private int x;
+	private int y;
+	
+	public Position(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+}
--- GLI/src/data/unit/Infantry.java
+++ GLI/src/data/unit/Infantry.java
@@ -4,7 +4,7 @@ import process.visitor.unit_visitor.UnitVisitor;
 
 public class Infantry extends Units {
 	private static final int BASE_HEALTH = 2;
-	private static final int RANGE = 0;
+	private static final int RANGE = 1;
 	private static final int MOVEMENT = 1;
 	private static final int DAMAGE = 0;
 	private static final int DEFENSE = 1;
