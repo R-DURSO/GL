1fc3918398e6e670e8994ddd31537135ba31ed4f
R-DURSO <60429677+R-DURSO@users.noreply.github.com>
2020-02-20 17:53:24 +0100

    Add files via upload
    mes push marche 1/10 sa devient chiant ...... sinon premier jet de la classe Start et me fallait mettre a jour pour récupérer Actionmanager

--- GLI/src/process/game/GameLoop.java
+++ GLI/src/process/game/GameLoop.java
@@ -2,13 +2,42 @@ package process.game;
 import java.util.ArrayList;
 
 import data.actions.*;
+import data.GameMap;
+import data.Power;
+import data.resource.*;
 public class GameLoop {
 	private ArrayList<Action> action[]= new ArrayList[8] ; 
-	private ArrayList<Action> action[]= new ArrayList<>()[8];
-	action[0]= new ArrayList<ActionMakeAlliance>() ;
-	 action[1]= new ArrayList<ActionBreakAlliance>(); 
-	public   GameLoop() {
-		
+
+	// constante temporaire 
+	private ActionPoints nbaction = new ActionPoints();
+	private Power power1 = new Power("player1");
+	private Power power2 = new Power("player2");
+	
+	
+	public   GameLoop( Power[] powers,  GameMap  map ) {
+		InitTurn();
+		for( int i=0 ; i<powers.length ;  i++) {
+
+			while(wouldturn((ActionPoints) powers[i].getResource(4))) {
+				// test des action possible 
+
+				
+			}
+		}
+	}
+	
+	public void InitTurn() {
+		for (int i=0; i<8; i++) {
+			action[i]= new ArrayList<Action>();
+		}
+
+	}
+	public Boolean  wouldturn( ActionPoints action) {
+		if( action.GetAction_Number() >0 ) { // on rajoute plus tard l'potion du bouton fin de tour 
+			return true;
+		}else {
+			return false;
+		}
 	}
 	
 }
