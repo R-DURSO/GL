050e51b3fc2f5d8bca55790a108a44f69972d964
Maxence <Maxence>
2020-02-20 20:34:54 +0100

    légère modif de l'UnitFactory

--- GLI/src/data/actions/ActionTypes.java
+++ GLI/src/data/actions/ActionTypes.java
@@ -6,7 +6,7 @@ public class ActionTypes {
 	private static final int ACTION_ATTACK = 2;
 	private static final int ACTION_MOVE = 3;
 	private static final int ACTION_CONSTRUCT = 4;
-	private static final int ACTION_CREAT_UNITS = 5;
+	private static final int ACTION_CREATE_UNITS = 5;
 	private static final int ACTION_DESTROY_BUILDING = 6;
 	private static final int ACTION_DESTROY_UNITS= 7;
 	
