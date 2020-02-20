a799b07b1fecde7def34a742b97a7f8cb6e62d92
R-DURSO <60429677+R-DURSO@users.noreply.github.com>
2020-02-20 09:48:19 +0100

    Add files via upload

---
+++ GLI/src/process/game/GameLoop.java
@@ -0,0 +1,14 @@
+package process.game;
+import java.util.ArrayList;
+
+import data.actions.*;
+public class GameLoop {
+	private ArrayList<Action> action[]= new ArrayList[8] ; 
+	private ArrayList<Action> action[]= new ArrayList<>()[8];
+	action[0]= new ArrayList<ActionMakeAlliance>() ;
+	 action[1]= new ArrayList<ActionBreakAlliance>(); 
+	public   GameLoop() {
+		
+	}
+	
+}
