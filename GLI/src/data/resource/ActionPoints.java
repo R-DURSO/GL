fa94b1794de70c751571001a61052406f4c42259
R-DURSO <rdurso@outlook.fr>
2020-02-20 17:47:58 +0100

    premier jet de la  classe Start et je devait mettre a jour pour pull :)

--- GLI/src/data/resource/ActionPoints.java
+++ GLI/src/data/resource/ActionPoints.java
@@ -3,10 +3,14 @@ package data.resource;
 import process.visitor.ressource_visitor.RessourceVisitor;
 
 public class ActionPoints extends Resource {
+	private int Action_Number = 3; 
+	private static final int Action_Max = 5 ; 
 	int variation () {
 		return 0;
 	}
-	
+	public int GetAction_Number() {
+		return Action_Number;
+	}
 	@Override
 	public <R> R accept(RessourceVisitor<R> visitor) {
 		return visitor.visit(this);
--- GLI/src/process/game/Start.java
+++ GLI/src/process/game/Start.java
@@ -1,9 +1,27 @@
 package process.game;
 
+import data.GameMap;
+import data.Power;
+import process.management.MapBuilder;
+import process.management.PowerFactory;
+
 public class Start {
-
-	public Start() {
-		// TODO Auto-generated constructor stub
+	private Power powers[]= new Power[4];
+	private GameMap map;
+	public Start(int nbplayer , int size , int waterAmout) {
+		initPlayer(nbplayer); 
+		generatedMap(size, waterAmout);	
+		new GameLoop( powers,  map);
 	}
-
+	public void initPlayer(int nbplayer){
+		for(int i = 0; i < nbplayer; i++) {
+			this.powers[i] = PowerFactory.createPower("Joueur " + (i + 1));
+		}
+	}
+	public void generatedMap(int size , int waterAmout) {
+		MapBuilder mb = new MapBuilder(size, waterAmout, powers);
+		mb.displayMap();
+		map = mb.buildMap();
+		
+	}
 }
