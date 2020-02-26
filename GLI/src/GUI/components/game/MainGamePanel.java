cd4f3f1fa70e2b41a9b16a5bac4c18aaed2fc975
AldricVS <aldric.silvestre@outlook.fr>
2020-02-26 12:21:35 +0100

    rien de vrai désolé

--- GLI/src/GUI/components/game/MainGamePanel.java
+++ GLI/src/GUI/components/game/MainGamePanel.java
@@ -14,47 +14,16 @@ import GUI.components.GuiPreferences;
 
 public class MainGamePanel extends JPanel{
 	private static final long serialVersionUID = -4989371043690170741L;
-	
-	private final Dimension DIMENSION_MAIN = 
-			new Dimension(GuiPreferences.WIDTH * GuiPreferences.GAME_MAIN_RATIO_SUBSTRACT_WIDTH / GuiPreferences.GAME_MAIN_RATIO_WIDTH,
-					GuiPreferences.HEIGHT * GuiPreferences.GAME_PANELS_SUBSTRACT_HEIGHT / GuiPreferences.GAME_PANELS_RATIO_HEIGHT);
-	private final Dimension DIMENSION_ASIDE = 
-			new Dimension((GuiPreferences.WIDTH / GuiPreferences.GAME_MAIN_RATIO_WIDTH) - 10,
-					GuiPreferences.HEIGHT * GuiPreferences.GAME_PANELS_SUBSTRACT_HEIGHT / GuiPreferences.GAME_PANELS_RATIO_HEIGHT);
-	private JPanel mapPanel = new JPanel();
-	private JPanel infosPanel = new JPanel();
-
 	public MainGamePanel() {
 		init();
 
 	}
 
 	private void init() {
-		setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
+		setLayout(new GridLayout(0,1));
 		setBorder(BorderFactory.createLineBorder(Color.BLACK, 2, true));
-		
-		initMapPanel();
-		initInfosPanel();
-		
-		add(mapPanel);
-		add(infosPanel);
+		setLayout(new GridLayout(0,1));
+		add(new JLabel("affichage de la map", SwingConstants.CENTER));
 	}
-
-	private void initMapPanel() {
-		mapPanel.setPreferredSize(DIMENSION_MAIN);
-		mapPanel.setLayout(new GridLayout(0,1));
-		mapPanel.add(new JLabel("affichage de la map", SwingConstants.CENTER));
-	}
-	
-	private void initInfosPanel() {
-		infosPanel.setPreferredSize(DIMENSION_ASIDE);
-		infosPanel.setLayout(new GridLayout(0,1));
-		infosPanel.add(new JLabel("<html><p>infos du joueur, affiche:</p>"
-				+ "<ul><li>liste des troupes (nombre de chaque unité par type)</li>"
-				+ "<li>liste des batiments (pareil)</li></ul>"
-				+ "</html>", SwingConstants.RIGHT));
-		infosPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
-	}
-
 
 }
