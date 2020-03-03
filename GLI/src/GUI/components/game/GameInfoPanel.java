5f8d0f9451f9c3bfeb386f2614155d067981e93b
Maxence <Maxence>
2020-03-03 16:30:50 +0100

    Rajout de la sélection de la box
    bon, préfaites, mais je vais rejouter les méthodes

--- GLI/src/GUI/components/game/GameInfoPanel.java
+++ GLI/src/GUI/components/game/GameInfoPanel.java
@@ -2,49 +2,55 @@ package GUI.components.game;
 
 import java.awt.GridLayout;
 
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import data.boxes.Box;
 import data.resource.Resource;
-import data.resource.ResourceCost;
 import data.resource.ResourceTypes;
+
+/**
+ * <p>The container for information from the Game.</p>
+ * <p>Use ResourcePanel and BoxSelectedPanel</p>
+ */
 
 public class GameInfoPanel extends JPanel{
 	private static final long serialVersionUID = 1700840665124988595L;
-	private Resource resources[];
 	
-	private JPanel positionPanel = new JPanel();
+	private BoxSelectedPanel positionPanel;
 	private ResourcePanel actionPointsPanel;
 	private ResourcePanel goldPanel;
 	private ResourcePanel stonePanel;
 	private ResourcePanel woodPanel;
 	private ResourcePanel foodPanel;
 
-	public GameInfoPanel(Resource resources[]) {
-		this.resources = resources;
+	public GameInfoPanel(Resource resources[], Box box) {
 		actionPointsPanel = new ResourcePanel("Points d'action", resources[ResourceTypes.RESOURCE_ACTIONS]);
 		goldPanel = new ResourcePanel("Or", resources[ResourceTypes.RESOURCE_GOLD]);
 		stonePanel = new ResourcePanel("Pierre", resources[ResourceTypes.RESOURCE_STONE]);
 		woodPanel = new ResourcePanel("Bois", resources[ResourceTypes.RESOURCE_WOOD]);
 		foodPanel = new ResourcePanel("Nourriture", resources[ResourceTypes.RESOURCE_FOOD]);
+		positionPanel = new BoxSelectedPanel(box);
 		init();
 	}
 
 	private void init() {
 		setLayout(new GridLayout(0,6));
-		
+		/**
 		//temporaire
 		positionPanel.setLayout(new GridLayout(0,1));
 		positionPanel.add(new JLabel("Position de la case sélectionée"));
-		
+		*/
 		add(positionPanel);
 		add(actionPointsPanel);
 		add(goldPanel);
 		add(stonePanel);
 		add(woodPanel);
 		add(foodPanel);
+	}
+	
+	public void refresh() {
+		
 	}
 
 }
