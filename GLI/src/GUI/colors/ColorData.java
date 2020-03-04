4b2f06799b26cc03fdfc91dd0aa17a85dc66addc
Maxence <mhenneke@sm1-458-10>
2020-03-04 15:57:28 +0100

    Les belles couleurs

--- GLI/src/GUI/colors/ColorData.java
+++ GLI/src/GUI/colors/ColorData.java
@@ -11,41 +11,41 @@ public class ColorData {
 	//power colors
 	
 	public static final Color NO_POWER_COLOR = Color.BLACK;
-	public static final Color POWER_1_COLOR = new Color(250, 50, 50);;
-	public static final Color POWER_2_COLOR = Color.YELLOW;
-	public static final Color POWER_3_COLOR = Color.GREEN;
-	public static final Color POWER_4_COLOR = Color.MAGENTA;
+	public static final Color POWER_1_COLOR = new Color(240, 50, 50);
+	public static final Color POWER_2_COLOR = new Color(250, 250, 25);
+	public static final Color POWER_3_COLOR = new Color(80, 250, 40);
+	public static final Color POWER_4_COLOR = new Color(200, 20, 250);
 	
 	//unit color
 	
-	public static final Color ARCHER_COLOR = Color.MAGENTA;
-	public static final Color BATTERING_RAM_COLOR = Color.MAGENTA;
-	public static final Color BOAT_COLOR = Color.MAGENTA;
-	public static final Color CAVALRY_COLOR = Color.MAGENTA;
-	public static final Color INFANTRY_COLOR = Color.MAGENTA;
-	public static final Color PIKEMAN_COLOR = Color.MAGENTA;
-	public static final Color TREBUCHET_COLOR = Color.MAGENTA;
+	public static final Color ARCHER_COLOR = new Color(235, 180, 200);
+	public static final Color BATTERING_RAM_COLOR = new Color(235, 180, 200);
+	public static final Color BOAT_COLOR = new Color(235, 180, 200);
+	public static final Color CAVALRY_COLOR = new Color(235, 180, 200);
+	public static final Color INFANTRY_COLOR = new Color(235, 180, 200);
+	public static final Color PIKEMAN_COLOR = new Color(235, 180, 200);
+	public static final Color TREBUCHET_COLOR = new Color(235, 180, 200);
 	
 	//building color
 	
-	public static final Color BARRACK_COLOR = Color.MAGENTA;
-	public static final Color DOCK_COLOR = Color.MAGENTA;
-	public static final Color WORKSHOP_COLOR = Color.MAGENTA;
-	public static final Color MINE_COLOR = Color.MAGENTA;
-	public static final Color QUARRY_COLOR = Color.MAGENTA;
-	public static final Color SAWMILL_COLOR = Color.MAGENTA;
-	public static final Color WINDMILL_COLOR = Color.MAGENTA;
-	public static final Color CAPITAL_COLOR = Color.PINK;
-	public static final Color DOOR_COLOR = Color.MAGENTA;
-	public static final Color TEMPLE_COLOR = Color.MAGENTA;
-	public static final Color WALL_COLOR = Color.MAGENTA;
+	public static final Color BARRACK_COLOR = new Color(90, 90, 90);
+	public static final Color DOCK_COLOR = new Color(90, 90, 90);
+	public static final Color WORKSHOP_COLOR = new Color(90, 90, 90);
+	public static final Color MINE_COLOR = new Color(90, 90, 90);
+	public static final Color QUARRY_COLOR = new Color(90, 90, 90);
+	public static final Color SAWMILL_COLOR = new Color(90, 90, 90);
+	public static final Color WINDMILL_COLOR = new Color(90, 90, 90);
+	public static final Color CAPITAL_COLOR = new Color(230, 20, 80);
+	public static final Color DOOR_COLOR = new Color(90, 90, 90);
+	public static final Color TEMPLE_COLOR = new Color(90, 90, 90);
+	public static final Color WALL_COLOR = new Color(90, 90, 90);
 	
 	//Resource color
 	
-	public static final Color FOOD_COLOR = Color.RED;
-	public static final Color GOLD_COLOR = Color.YELLOW;
-	public static final Color STONE_COLOR = Color.GRAY;
-	public static final Color WOOD_COLOR = new Color(45, 21, 0);
-	public static final Color ARTIFACT_COLOR = Color.MAGENTA;
+	public static final Color FOOD_COLOR = new Color(255, 120, 100);
+	public static final Color GOLD_COLOR = new Color(220, 200, 40);
+	public static final Color STONE_COLOR = new Color(170, 170, 170);;
+	public static final Color WOOD_COLOR = new Color(140, 70, 20);
+	public static final Color ARTIFACT_COLOR = Color.WHITE;
 
 }
