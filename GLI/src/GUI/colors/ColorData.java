11aa55a8d425cf7dd24cf2cf977c8c002578b6fd
Maxence <mhenneke@sm1-458-10>
2020-03-04 15:31:31 +0100

    la belle bleue !

--- GLI/src/GUI/colors/ColorData.java
+++ GLI/src/GUI/colors/ColorData.java
@@ -4,21 +4,16 @@ import java.awt.Color;
 
 public class ColorData {
 
-	public static final Color WATER_COLOR = new Color(156, 211, 219); // #9cd3db
-	
-	public static final Color GROUND_COLOR = new Color(155, 118, 83); // #9b7653
+	public static final Color WATER_COLOR = new Color(25, 190, 200); // #053caa
+	public static final Color GROUND_COLOR = new Color(50, 168, 109); // 32a86d
 	
 	
 	//power colors
 	
 	public static final Color NO_POWER_COLOR = Color.BLACK;
-	
 	public static final Color POWER_1_COLOR = Color.RED;
-	
 	public static final Color POWER_2_COLOR = Color.YELLOW;
-	
 	public static final Color POWER_3_COLOR = Color.GREEN;
-	
 	public static final Color POWER_4_COLOR = Color.MAGENTA;
 	
 	//unit color
