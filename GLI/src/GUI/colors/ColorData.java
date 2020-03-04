f00c44cc74fd8f87543cc001344cc385f80a71e4
Maxence <mhenneke@sm1-458-10>
2020-03-04 15:35:08 +0100

    la belle verte

--- GLI/src/GUI/colors/ColorData.java
+++ GLI/src/GUI/colors/ColorData.java
@@ -5,13 +5,13 @@ import java.awt.Color;
 public class ColorData {
 
 	public static final Color WATER_COLOR = new Color(25, 190, 200); // #053caa
-	public static final Color GROUND_COLOR = new Color(50, 168, 109); // 32a86d
+	public static final Color GROUND_COLOR = new Color(45, 186, 94); // 32a86d
 	
 	
 	//power colors
 	
 	public static final Color NO_POWER_COLOR = Color.BLACK;
-	public static final Color POWER_1_COLOR = Color.RED;
+	public static final Color POWER_1_COLOR = new Color(250, 50, 50);;
 	public static final Color POWER_2_COLOR = Color.YELLOW;
 	public static final Color POWER_3_COLOR = Color.GREEN;
 	public static final Color POWER_4_COLOR = Color.MAGENTA;
