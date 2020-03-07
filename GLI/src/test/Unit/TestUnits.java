6b7d06a9641925bd9e284f7afefdaacdca7bf96c
Maxence <Maxence>
2020-03-07 22:04:52 +0100

    Unit test cout

--- GLI/src/test/Unit/TestUnits.java
+++ GLI/src/test/Unit/TestUnits.java
@@ -8,11 +8,12 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import data.GameMap;
+import data.InitialValue;
 import data.Position;
 import data.Power;
 import data.boxes.*;
+import data.resource.ResourceTypes;
 import data.unit.*;
-import process.management.ActionValidator;
 import process.management.MapBuilder;
 import process.management.UnitManager;
 
@@ -25,8 +26,8 @@ import process.management.UnitManager;
 public class TestUnits {
 	private static Power powers[] = new Power[2];
 	private static GameMap map;
-	private static ActionValidator Action;
 	private static Position target;
+	private static Power power;
 	
 	@BeforeClass
 	public static void prepareMap() {
@@ -34,7 +35,6 @@ public class TestUnits {
 		powers[1] = new Power("j2");
 		MapBuilder mapBuilder = new MapBuilder(4, 0, powers);
 		map = mapBuilder.buildMap();
-		Action = new ActionValidator(map);
 	}
 	
 	@Test(expected = AssertionError.class)
@@ -55,5 +55,24 @@ public class TestUnits {
 	public void createUnitUnknown() {
 		target = new Position(0,1);
 		UnitManager.getInstance().addUnits(powers[0], map.getBox(target), 8008135, 5);
+	}
+	
+	/*
+	 * Actuellement, le cout en Or se fait dans l'ActionValidator...
+	 * Faudrait surement déplacer ça
+	@Test
+	public void createUnitCost() {
+		power = powers[0];
+		target = new Position(0,1);
+		UnitManager.getInstance().addUnits(powers[0], map.getBox(target), UnitTypes.UNIT_INFANTRY, 5);
+		assertEquals(InitialValue.GOLD_INITAL_VALUE - (Infantry.COST * 5), power.getResourceAmount(ResourceTypes.RESOURCE_GOLD));
+	}
+	*/
+	
+	@Test
+	public void createUnitFoodCost() {
+		power = powers[0];
+		target = new Position(0,1);
+		UnitManager.getInstance().addUnits(powers[0], map.getBox(target), UnitTypes.UNIT_INFANTRY, 5);
 	}
 }
