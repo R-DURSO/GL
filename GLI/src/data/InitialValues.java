12bbfe9936c514afd94e7cd0080dbb6792b93216
Maxence <Maxence@192.168.1.19>
2020-04-16 17:58:23 +0200

    changement début (sur-nerfed)

--- GLI/src/data/InitialValues.java
+++ GLI/src/data/InitialValues.java
@@ -11,10 +11,10 @@ import java.io.Serializable;
 public class InitialValues implements Serializable {
 	public static final int NUMBER_INITIAL_VALUE = 500 ;
 	public static final int ACTION_INITIAL_VALUE = 2;
-	public static final int WOOD_INITIAL_VALUE = 150;
+	public static final int WOOD_INITIAL_VALUE = 200;
 	public static final int GOLD_INITIAL_VALUE = 100;
-	public static final int FOOD_INITIAL_VALUE = 200;
-	public static final int STONE_INITIAL_VALUE = 50;
+	public static final int FOOD_INITIAL_VALUE = 100;
+	public static final int STONE_INITIAL_VALUE = 0;
 	public static final int SCORE_INITIAL_VALUE = 0;
 
 	public static final int ACTION_BASE_PRODUCTION = 2;
